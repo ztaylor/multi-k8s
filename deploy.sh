b731688d349339e719a3f796e40e3a69e0c1363e
Zachary Taylor <zachary.taylor@gmail.com>
2020-01-09 19:13:38 -0800

    pushes built docker images to :latest tag in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 docker build -t ztaylor510/multi-client:latest -t ztaylor510/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t ztaylor510/multi-server:latest -t ztaylor510/multi-client:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t ztaylor510/multi-worker:latest -t ztaylor510/multi-client:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push ztaylor510/multi-client:latest
+docker push ztaylor510/multi-server:latest
+docker push ztaylor510/multi-worker:latest
 docker push ztaylor510/multi-client:$GIT_SHA
 docker push ztaylor510/multi-server:$GIT_SHA
 docker push ztaylor510/multi-worker:$GIT_SHA
