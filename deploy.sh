cfe6c5289ae2c43c31cfc9a2e2a9608dc901cce1
Zachary Taylor <zachary.taylor@gmail.com>
2020-01-09 19:11:39 -0800

    updates docker build commands in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t ztaylor510/multi-client:latest -t ztaylor510/multi-client:$GIT_SHA ./client/Dockerfile ./client
-docker build -t ztaylor510/multi-server:latest -t ztaylor510/multi-client:$GIT_SHA ./server/Dockerfile ./server
-docker build -t ztaylor510/multi-worker:latest -t ztaylor510/multi-client:$GIT_SHA ./worker/Dockerfile ./worker
+docker build -t ztaylor510/multi-client:latest -t ztaylor510/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t ztaylor510/multi-server:latest -t ztaylor510/multi-client:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t ztaylor510/multi-worker:latest -t ztaylor510/multi-client:$GIT_SHA -f ./worker/Dockerfile ./worker
 docker push ztaylor510/multi-client:$GIT_SHA
 docker push ztaylor510/multi-server:$GIT_SHA
 docker push ztaylor510/multi-worker:$GIT_SHA
